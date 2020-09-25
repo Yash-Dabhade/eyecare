de730074149660c47dd7a7939cadf6ca14a01195
YASH DABHADE <64470535+Yash-Dabhade@users.noreply.github.com>
2020-09-25 22:03:05 +0530

    Add files via upload

--- icare.py
+++ icare.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
             message='Follow 20-20-20 Rule Now. Please follow this rule to keep your eyes in good health',
             title='Look at a faraway object for 20 sec.',
             app_icon='icon2.ico',
-            timeout=10
+            timeout=21
         )
         time.sleep(60*20)
